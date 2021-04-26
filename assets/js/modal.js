abc800839e7f4cb022c7cc1ae17d8dca60d2d3ea
Gor Darbinyan <darbinyangor1@gmail.com>
2021-04-26 23:35:54 +0400

    add JS code on modal-login

---
+++ assets/js/modal.js
@@ -0,0 +1,46 @@
+var link = document.querySelector(".login-link");
+    popup = document.querySelector(".modal-login");
+    close = popup.querySelector(".modal-close");
+    form = popup.querySelector("form");
+    login = popup.querySelector("[name=login]");
+    password = popup.querySelector("[name=password]");
+    isStorageSupport = true;
+    storage = "";
+    try {
+        storage = localStorage.getItem("login");
+    } catch (err) {
+        isStorageSupport = false;
+    }
+    link.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("modal-show");
+        if (storage) {
+            login.value = storage;
+            password.focus();
+        } else{
+            login.focus();
+        }
+    });
+    close.addEventListener("click", function (evt){
+        evt.preventDefault();
+        popup.classList.remove("modal-show");
+    });
+    form.addEventListener("submit", function (evt){
+        if (!login.value || !password.value) {
+            evt.preventDefault();
+            console.log("You need to enter a username or password");
+        } else {
+            if (isStorageSupport) {
+                localStorage.setItem("login", login.value);
+            }
+        }
+    });    
+    window.addEventListener("keydown", function (evt) {
+        if (evt.keyCode === 27) {
+            evt.preventDefault();
+            if(popup.classList.contains("modal-show")){
+                popup.classList.remove("modal-show");
+            }
+        }
+    });
+
